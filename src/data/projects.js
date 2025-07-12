22d2c86ad8a6eec89c1764aa4c7fd1dffd06be07
cindy kivala <cindykivala@gmail.com>
2025-07-12 16:46:23 +0300

    lists + keys

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    name: "Project One",
+    about: "This is the first project.",
+    technologies: ["React", "JavaScript"]
+  },
+  {
+    id: 2,
+    name: "Project Two",
+    about: "This is the second project.",
+    technologies: ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    id: 3,
+    name: "Project Three",
+    about: "This is the third project.",
+    technologies: ["Node.js", "Express", "MongoDB"]
+  }
+];
+
+export default projects;
